4d64bf41d977fb93b187115e86bdc0201e2093c2
thinh.le <roshan1234.lol@gmail.com>
2022-07-20 08:23:03 +0700

    [server] Init http server 20 Jul

--- http-server/index.js
+++ http-server/index.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const { httpErrorHandler } = require('./error-handler');
 const { configureRouting } = require('./routing-config');
 
+// App initialization
 const app = express();
 
 // Base configuration
@@ -12,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Register route
 httpErrorHandler(app);
 configureRouting(app);
 
-module.exports = app;
+module.exports = http.createServer(app);
